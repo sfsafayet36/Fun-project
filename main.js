d803167247bda49cacb3b092ea8403157e15db21
sf.safayet36@gmail.com <sf.safayet36@gmail.com>
2023-05-16 13:37:25 +0600

    first

---
+++ main.js
@@ -0,0 +1,166 @@
+/* Dipjol Character code */
+
+
+function cliCk(){
+  document.getElementById('para').innerHTML="You are now dipjol"
+document.getElementById('dipjol-img').style.transform="scale(1.1)"
+  
+  document.getElementById('dipjol-img').style.transition="0.3s ease"
+  
+  document.getElementById('manna-img').style.transform = "scale(1)"
+  
+  
+  document.getElementById('dipjol-img').style.border = "5px solid red"
+  
+  document.getElementById('manna-img').style.border = "4px solid white"
+ }
+
+/* Manna Character Code */
+
+function clicK() {
+  document.getElementById('para').innerHTML = "You are now Manna"
+
+
+document.getElementById('manna-img').style.transform = "scale(1.1)"
+
+document.getElementById('manna-img').style.transition = "0.3s ease"
+
+document.getElementById('dipjol-img').style.transform = "scale(1)"
+
+document.getElementById('manna-img').style.border = "5px solid red"
+
+document.getElementById('dipjol-img').style.border = "4px solid white"
+}
+
+/* Cows Code */
+
+function imgClick() {
+  document.getElementById('img-1').style.display = "inline-block"
+
+  document.getElementById('img-9').style.display = "none"
+  
+
+  }
+  
+  function imgClick2() {
+    document.getElementById('img-2').style.display = "inline-block"
+  
+    document.getElementById('img-10').style.display = "none"
+  
+  
+  }
+
+function imgClick3() {
+  document.getElementById('img-3').style.display = "inline-block"
+
+  document.getElementById('img-11').style.display = "none"
+
+
+}
+
+
+function imgClick4() {
+  document.getElementById('img-4').style.display = "inline-block"
+
+  document.getElementById('img-12').style.display = "none"
+
+
+}
+
+ function imgClick5() {
+  document.getElementById('img-5').style.display = "inline-block"
+
+  document.getElementById('img-13').style.display = "none"
+
+
+}
+
+function imgClick6() {
+  document.getElementById('img-6').style.display = "inline-block"
+
+  document.getElementById('img-14').style.display = "none"
+
+
+}
+
+function imgClick7() {
+  document.getElementById('img-7').style.display = "inline-block"
+
+  document.getElementById('img-15').style.display = "none"
+
+
+}
+
+function imgClick8() {
+  document.getElementById('img-8').style.display = "inline-block"
+
+  document.getElementById('img-16').style.display = "none"
+
+}
+
+/* Cow transform */
+
+
+function imgclicK() {
+  document.getElementById('img-1').style.display = "none"
+
+  document.getElementById('img-9').style.display = "inline-block"
+
+
+}
+
+function imgclicK2() {
+  document.getElementById('img-2').style.display = "none"
+
+  document.getElementById('img-10').style.display = "inline-block"
+
+
+}
+
+function imgclicK3() {
+  document.getElementById('img-3').style.display = "none"
+
+  document.getElementById('img-11').style.display = "inline-block"
+
+
+}
+
+
+function imgclicK4() {
+  document.getElementById('img-4').style.display = "none"
+
+  document.getElementById('img-12').style.display = "inline-block"
+
+
+}
+
+function imgclicK5() {
+  document.getElementById('img-5').style.display = "none"
+
+  document.getElementById('img-13').style.display = "inline-block"
+
+
+}
+
+function imgclicK6() {
+  document.getElementById('img-6').style.display = "none"
+
+  document.getElementById('img-14').style.display = "inline-block"
+
+
+}
+
+function imgclicK7() {
+  document.getElementById('img-7').style.display = "none"
+
+  document.getElementById('img-15').style.display = "inline-block"
+
+
+}
+
+function imgclicK8() {
+  document.getElementById('img-8').style.display = "none"
+
+  document.getElementById('img-16').style.display = "inline-block"
+
+}
